e9d06510e624966d3844ed1e8955f82c955cd2fd
RedZvER <6995335+RedZvER@users.noreply.github.com>
2024-07-01 04:49:10 +0500

    Start comics done

--- Assets/Scripts/Game/GoblinComicsManager.cs
+++ Assets/Scripts/Game/GoblinComicsManager.cs
@@ -4,35 +4,97 @@ namespace GoblinzMechanics.Game
     using System.Collections.Generic;
     using GoblinzMechanics.Utils;
     using UnityEngine;
+    using UnityEngine.Events;
 
     public class GoblinComicsManager : Singleton<GoblinComicsManager>
     {
-        [SerializeField] private AudioSource comicsSource;
+        [SerializeField] private AudioSource _comicsSource;
+        [SerializeField] private float _fadeOutDuration = 0.5f;
         [SerializeField] private List<ComicsFrame> _startGameComics = new();
+        [SerializeField] private List<ComicsFrame> _startSecondGameComics = new();
+
+        public UnityEvent OnAllComicsDone;
+
+        private void Start()
+        {
+            StartCoroutine(StartGamePlay());
+        }
 
         public IEnumerator StartGamePlay()
         {
-            foreach (var frame in _startGameComics)
+            if (GoblinGameStats.InstanceNonNull)
             {
-                frame.image.color = Color.black;
+                OnAllComicsDone?.Invoke();
+                yield break;
+            }
+
+            yield return PlayComics(_startGameComics);
+            yield return PlayComics(_startSecondGameComics);
+            OnAllComicsDone?.Invoke();
+        }
+
+        private IEnumerator PlayComics(List<ComicsFrame> comics)
+        {
+            foreach (var frame in comics)
+            {
+                frame.image.color = new Color(0, 0, 0, 0);
+                frame.gameObject.SetActive(true);
+            }
+
+            float frameTimer = 0f;
+            while (frameTimer <= _fadeOutDuration)
+            {
+                foreach (var frame in comics)
+                {
+                    frame.image.color = Color.Lerp(new Color(0, 0, 0, 0), Color.black, frameTimer / _fadeOutDuration);
+
+                }
+                frameTimer += Time.deltaTime;
                 yield return null;
             }
-            float frameTimer;
-            foreach (var frame in _startGameComics)
+            foreach (var frame in comics)
+            {
+                frame.image.color = Color.black;
+            }
+            foreach (var frame in comics)
             {
                 frameTimer = 0;
-                if(comicsSource != null && frame.audioClip != null) {
-                    comicsSource.PlayOneShot(frame.audioClip);
-                }
-                while(frameTimer <= frame.duration) {
-                    frame.image.color = Color.Lerp(Color.black, Color.white, frameTimer / frame.duration);
+                while (frameTimer <= _fadeOutDuration)
+                {
+                    frame.image.color = Color.Lerp(Color.black, Color.white, frameTimer / _fadeOutDuration);
                     frameTimer += Time.deltaTime;
                     yield return null;
                 }
-                if(comicsSource != null) {
-                    comicsSource.Stop();
-                }
+
                 frame.image.color = Color.white;
+                if (_comicsSource != null && frame.audioClip != null)
+                {
+                    _comicsSource.PlayOneShot(frame.audioClip);
+                }
+
+                yield return new WaitForSeconds(frame.duration);
+
+                if (_comicsSource != null)
+                {
+                    _comicsSource.Stop();
+                }
+            }
+
+            frameTimer = 0f;
+            while (frameTimer <= _fadeOutDuration)
+            {
+                foreach (var frame in comics)
+                {
+                    frame.image.color = Color.Lerp(Color.white, new Color(0, 0, 0, 0), frameTimer / _fadeOutDuration);
+                }
+                frameTimer += Time.deltaTime;
+                yield return null;
+            }
+
+            foreach (var frame in comics)
+            {
+                frame.image.color = new Color(0, 0, 0, 0);
+                frame.gameObject.SetActive(false);
             }
         }
     }
